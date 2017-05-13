bae8cc8d5a4d1db493928883e487a8d91e122a90
Panagiotis-Stephanos Makos <psmakos@hotmail.com>
2017-05-13 16:51:44 +0300

    Add NFC libraries, ReMake UI

---
+++ MX2T Project/DonatePage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using ZXing;
+using ZXing.Mobile;
+using Pcsc;
+using Windows.Devices.SmartCards;
+using System.Threading.Tasks;
+using Pcsc.Common;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MX2T_Project
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DonatePage : Page
+    {
+        private MobileBarcodeScanner _scanner;
+        public DonatePage()
+        {
+            this.InitializeComponent();
+        }
+
+        SmartCardReader m_cardReader;
+
+        private async void QR_Search_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            _scanner = new MobileBarcodeScanner(this.Dispatcher);
+            _scanner.UseCustomOverlay = false;
+            _scanner.TopText = "Hold camera up to QR code";
+            _scanner.BottomText = "Camera will automatically scan QR code";
+
+            var result = await _scanner.Scan();
+            ProcessScanResult(result);
+        }
+        private void ProcessScanResult(Result result)
+        {
+
+            MessageDialog myMessage = new MessageDialog("Συγχρονιστήκατε με τον αποδοχέα " + result.Text, "Συγχρονισμός");
+            myMessage.ShowAsync();
+        }
+
+        private async void NFC_Search_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+
+            var result = await _scanner.Scan();
+            ProcessScanResult(result);
+        }
+    }
+}
--- MX2T Project/MainPage.xaml.cs
+++ MX2T Project/MainPage.xaml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,40 +12,60 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using ZXing;
-using ZXing.Mobile;
+using Windows.UI;//For titlebar
+using Windows.UI.ViewManagement;//For titlebar
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace MX2T_Project
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
-        private MobileBarcodeScanner _scanner;
         public MainPage()
         {
             this.InitializeComponent();
 
         }
 
-        private async void QR_Search_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            _scanner = new MobileBarcodeScanner(this.Dispatcher);
-            _scanner.UseCustomOverlay = false;
-            _scanner.TopText = "Hold camera up to QR code";
-            _scanner.BottomText = "Camera will automatically scan QR code";
 
-            var result = await _scanner.Scan();
-            ProcessScanResult(result);
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            SetTitleBarBackground();
+            MyFrame.Navigate(typeof(DonatePage));
+
         }
-        private void ProcessScanResult(Result result)
-        {
+  
 
-            MessageDialog myMessage = new MessageDialog("Συγχρονιστήκατε με τον αποδοχέα " + result.Text, "Συγχρονισμός");
-            myMessage.ShowAsync();
+        private void SetTitleBarBackground()
+        {
+            // Get the instance of the Title Bar
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            // Set the color of the Title Bar content
+            titleBar.BackgroundColor = Colors.Teal;
+            titleBar.ForegroundColor = Colors.White;
+
+            // Set the color of the Title Bar buttons
+            titleBar.ButtonBackgroundColor = Colors.Teal;
+            titleBar.ButtonForegroundColor = Colors.White;
+        }
+
+        private void GoToDonate(object sender, TappedRoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(DonatePage));
+        }
+
+        private void GoToHistory(object sender, TappedRoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(HistoryPage));
+        }
+
+        private void GoToDashboard(object sender, TappedRoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(DashboardPage));
         }
     }
 }
