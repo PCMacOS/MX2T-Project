889a7d6b3886affe59fa877216e82ce0ce0c10f3
Panagiotis-Stephanos Makos <psmakos@hotmail.com>
2017-05-13 13:01:24 +0300

    Add QR Scaner

--- MX2T Project/MainPage.xaml.cs
+++ MX2T Project/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ZXing;
+using ZXing.Mobile;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +25,28 @@ namespace MX2T_Project
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MobileBarcodeScanner _scanner;
         public MainPage()
         {
             this.InitializeComponent();
+
+        }
+
+        private async void QR_Search_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            _scanner = new MobileBarcodeScanner(this.Dispatcher);
+            _scanner.UseCustomOverlay = false;
+            _scanner.TopText = "Hold camera up to QR code";
+            _scanner.BottomText = "Camera will automatically scan QR code";
+
+            var result = await _scanner.Scan();
+            ProcessScanResult(result);
+        }
+        private void ProcessScanResult(Result result)
+        {
+
+            MessageDialog myMessage = new MessageDialog("Συγχρονιστήκατε με τον αποδοχέα " + result.Text, "Συγχρονισμός");
+            myMessage.ShowAsync();
         }
     }
 }
